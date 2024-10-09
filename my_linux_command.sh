4ecf9862d2e6eda8513d5e9c0a0de4b7d5a22d05
Abdur Rakib <abdurrakib961@gmail.com>
2024-10-10 03:31:33 +0600

    feat: add script to fetch data using curl and check request success

--- my_linux_command.sh
+++ my_linux_command.sh
@@ -1,47 +1,54 @@
 #!/bin/bash
-## Go to users home directory
-# cd ~
+# Go to users home directory
+cd ~
 
-## Go to users home directory
-# cd ~
+# Go to root directory
+cd /
 
-## In users home directory create a new folder and move inside the folder
-# mkdir my_folder
-# cd my_folder
+# In users home directory create a new folder and move inside the folder
+mkdir my_folder
+cd my_folder
 
-## Create a new text file inside new directory
-# touch my_file.txt
+# Create a new text file inside new directory
+touch my_file.txt
 
-## Go to previous directory
-# cd ..
+# Go to previous directory
+cd ..
 
-## Remove the directory
-# rm -rf my_folder
+# Remove the directory
+rm -rf my_folder
 
-## Create the shell script file named my_linux_command.sh
-# nano my_linux_command.sh
+# Create the shell script file named my_linux_command.sh
+nano my_linux_command.sh
 
 # Inside my_linux_command.sh, write: hello world
-#!/bin/bash
 echo "hello world"
 
-## Save and exit the editor
-## Make the script executable
-## chmod +x my_linux_command.sh
+# Save and exit the editor
+# Make the script executable
+chmod +x my_linux_command.sh
 
-## Run the script
-# ./my_linux_command.sh
+# Run the script
+./my_linux_command.sh
 
-## Set an environment variable namely age with value 30 and in the .sh file access the variable and echo it
-#!/bin/bash
+# Set an environment variable namely age with value 30 and in the .sh file access the variable and echo it
 export age=30
 echo "Age is: $age"
 
-## Now set global environment variable namely experience with value 4 and echo it from the .sh file
-# nano ~/.bashrc
-# export experience=4
-# source ~/.bashrc
+# Now set global environment variable namely experience with value 4 and echo it from the .sh file
+nano ~/.bashrc
+export experience=4
+source ~/.bashrc
 echo "Experience is: $experience"
 
-## Generate a ssh keypair with keygen
-# ssh-keygen -t rsa -b 4096 -C "abdurrakib961@gmail.com"
+# Generate a ssh keypair with keygen
+ssh-keygen -t rsa -b 4096 -C "abdurrakib961@gmail.com"
+
+# Send a GET request to www.google.com using curl
+response=$(curl -o /dev/null -s -w "%{http_code}" https://www.google.com)
+
+if [ $response -eq 200 ]; then
+   echo "Success"
+else
+   echo "Fail"
+fi
