9ac2408ce090a6b2e7a34cc474423b59ea1f9190
Abdur Rakib <abdurrakib961@gmail.com>
2024-10-09 19:46:03 +0600

    feat: Navigate and manipulate directories in the user's home

--- my_linux_command.sh
+++ my_linux_command.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
+# Go to users home directory
+cd ~
+
+# Go to root directory
+cd /
+
+# In users home directory create a new folder and move inside the folder
+mkdir my_folder
+cd my_folder
+
+# Create a new text file inside new directory
+touch my_file.txt
+
+# Go to previous directory
+cd ..
+
+# Remove the directory
+rm -rf my_folder
+
 echo "Hello, World!"
