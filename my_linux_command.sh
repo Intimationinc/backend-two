7357f231bc025a270b849f34084938a7f655b3d5
Abdur Rakib <abdurrakib961@gmail.com>
2024-10-09 19:53:24 +0600

    feat: add hello world shell script with shebang

--- my_linux_command.sh
+++ my_linux_command.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
-# Go to users home directory
-cd ~
+## Go to users home directory
+# cd ~
 
-# Go to root directory
-cd /
+## Go to users home directory
+# cd ~
 
-# In users home directory create a new folder and move inside the folder
-mkdir my_folder
-cd my_folder
+## In users home directory create a new folder and move inside the folder
+# mkdir my_folder
+# cd my_folder
 
-# Create a new text file inside new directory
-touch my_file.txt
+## Create a new text file inside new directory
+# touch my_file.txt
 
-# Go to previous directory
-cd ..
+## Go to previous directory
+# cd ..
 
-# Remove the directory
-rm -rf my_folder
+## Remove the directory
+# rm -rf my_folder
 
-echo "Hello, World!"
+## Create the shell script file named my_linux_command.sh
+# nano my_linux_command.sh
+
+# Inside my_linux_command.sh, write: hello world
+#!/bin/bash
+echo "hello world"
+
+## Save and exit the editor
+## Make the script executable
+## chmod +x my_linux_command.sh
+
+## Run the script
+# ./my_linux_command.sh
