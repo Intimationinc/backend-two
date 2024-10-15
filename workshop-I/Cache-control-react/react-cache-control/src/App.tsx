3e95814b2e8c1cda5f7e7ab5df76f541bc66638b
Hasan-Uz-Zaman <hasanashik100@gmail.com>
2024-10-15 13:54:20 +0600

    Added Workshop-1 3 tasks

---
+++ workshop-I/Cache-control-react/react-cache-control/src/App.tsx
@@ -0,0 +1,13 @@
+import TodoList from "./TodoList";
+import WebSocketComponent from "./WebSocketComponent";
+
+function App() {
+  return (
+    <>
+      {/* <TodoList></TodoList> */}
+      <WebSocketComponent />
+    </>
+  );
+}
+
+export default App;
