9cf7c7362cf62099e5fdde6a7e5c79a966850743
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2024-10-17 21:32:02 +0600

    try to add public channel

--- workshop_1/task_02/client/src/App.jsx
+++ workshop_1/task_02/client/src/App.jsx
@@ -4,18 +4,18 @@ import { io } from "socket.io-client";
 function App() {
   const socket = useMemo(() => io("http://localhost:5500"), []);
   const [message, setMessage] = useState("");
-  const [room, setRoom] = useState("");
-  const [socketId, setSocketId] = useState("")
+  const [room, setRoom] = useState({ type: "", roomName: "" });
+  const [socketId, setSocketId] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    socket.emit("message", {message, room});
-    setMessage("")
+    socket.emit("message", { message, room: room.roomName });
+    setMessage("");
   };
 
   useEffect(() => {
     socket.on("connect", () => {
-      setSocketId(socket.id)
+      setSocketId(socket.id);
       console.log("connected", socket.id);
     });
     socket.on("welcome", (s) => {
@@ -50,16 +50,32 @@ function App() {
         />
         <div className="flex gap-2 items-center">
           <input
-            placeholder="room number"
+            placeholder="private room"
             type="text"
             onChange={(e) => setRoom(e.target.value)}
             className="border p-3 rounded-md flex-1"
           />
           <button
-            className="px-3 py-2.5 bg-blue-800 text-white rounded-md text-xl flex-1"
+            className="px-3 py-2.5 bg-blue-800 text-white rounded-md text-xl"
             type="submit"
           >
             send
+          </button>
+        </div>
+      </form>
+      <form>
+        <div className="flex gap-2 items-center">
+          <input
+            placeholder="public room"
+            type="text"
+            onChange={(e) => setRoom(e.target.value)}
+            className="border p-3 rounded-md flex-1"
+          />
+          <button
+            className="px-3 py-2.5 bg-blue-800 text-white rounded-md text-xl"
+            type="submit"
+          >
+            join
           </button>
         </div>
       </form>
