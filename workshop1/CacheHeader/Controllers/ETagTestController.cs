33a865a5e26d5d22d33be8dbbf86d99f1f9e8dc9
Muhammad Ishaan <iftekharctgbd@gmail.com>
2024-10-18 20:55:11 +0600

    feat: add cache header

---
+++ workshop1/CacheHeader/Controllers/ETagTestController.cs
@@ -0,0 +1,22 @@
+﻿using CacheHeader.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CacheHeader.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ETagTestController : ControllerBase
+{
+    [HttpGet]
+    [ETagFilter]
+    public IActionResult GetItem()
+    {
+        var data = new
+        {
+            name = "Test Name",
+            age = 24
+        };
+
+        return Ok(data);
+    }
+}
