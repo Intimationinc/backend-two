e46bab91e9e1231fd63a8c005f95668eca988415
Muhammad Ishaan <iftekharctgbd@gmail.com>
2024-10-25 19:04:45 +0600

    feat: add rwmutex

---
+++ workshop2/RWMutex/Program.cs
@@ -0,0 +1,46 @@
+﻿var sharedCounter = 0;
+
+var rwLock = new ReaderWriterLockSlim();
+
+var tasks = new List<Task>();
+for (var i = 0; i < 5; i++)
+{
+    tasks.Add(Task.Run(ReadWithRwMutex));
+    tasks.Add(Task.Run(WriteWithRwMutex));
+}
+
+await Task.WhenAll(tasks);
+
+Console.WriteLine("All tasks completed");
+
+void ReadWithRwMutex()
+{
+    rwLock.EnterReadLock();
+    try
+    {
+        Console.WriteLine($"Reading data by {Environment.CurrentManagedThreadId}");
+        Console.WriteLine($"Counter value: {sharedCounter}");
+        Task.Delay(TimeSpan.FromSeconds(3));
+    }
+    finally
+    {
+        Console.WriteLine($"Read lock releasing for {Environment.CurrentManagedThreadId}");
+        rwLock.ExitReadLock();
+    }
+}
+
+void WriteWithRwMutex()
+{
+    rwLock.EnterWriteLock();
+    try
+    {
+        Console.WriteLine($"Writing data by {Environment.CurrentManagedThreadId}");
+        sharedCounter++;
+        Task.Delay(TimeSpan.FromSeconds(3));
+    }
+    finally
+    {
+        Console.WriteLine($"Write lock releasing for {Environment.CurrentManagedThreadId}");
+        rwLock.ExitWriteLock();
+    }
+}
