d1086190bc05153915a82df9c84d0ec89b3c6cc2
123Somrat <jafaruddin875@gmail.com>
2024-11-05 21:07:41 +0100

    solved(case_study_3) : Solved solid principal case study 3

--- Workshop-3/Solid_Principal_Case_Studies/Case_Study_3.js
+++ Workshop-3/Solid_Principal_Case_Studies/Case_Study_3.js
@@ -1,1 +1,91 @@
-
+// RealTime seat update
+class RealTimeSeatUpdate {
+   updateSeat() {
+     console.log("update the seat");
+   }
+ }
+ 
+ 
+ // To do all events related task
+ class Events {
+   availableEvents() {
+     console.log("return all events");
+   }
+   bookAEvents(id) {
+     return "Booked events id and seat id";
+   }
+   availableSeatDependsOnEvents(eventName) {
+     return "available seat number on specific events";
+   }
+   cancelEvenets(id) {
+     return "Evenst cancel or not";
+   }
+ }
+ 
+ // for Payment
+ class Payment {
+   payment(amount) {}
+   refund(amount) {}
+ }
+ 
+ class Notification {
+   sendNotification() {}
+ }
+ 
+ 
+ // To do all events related task
+ class EventManagement {
+   constructor(
+     event,
+     realTimeSeatUpdate,
+     payment,
+  
+     notification
+   ) {
+     this.event = event;
+     this.realTimeSeatUpdate = realTimeSeatUpdate;
+     this.payment = payment;
+   
+     this.notification = notification;
+   }
+   availableEvents() {
+     const events = this.event.availableEvents();
+     console.log("available events");
+     return events;
+   }
+   reservation(id) {
+     const reservedSeatId = this.bookASeat.bookASeat(id);
+     const updateSeat = this.realTimeSeatUpdate.updateSeat();
+     const sendNotifications = this.notification.sendNotification(userId);
+     return {
+       reservedSeatId: reservedSeatId,
+       sendNotifications: sendNotifications,
+     };
+   }
+   payForTickets(amount) {
+     return "payment succesfull";
+   }
+   cancelEvent(id) {
+     console.log("cancel events id");
+   }
+   notification(userId) {
+     return "notifications received";
+   }
+ }
+ 
+ 
+ // Craete the Evenst instance and inject all needed dependencies
+ function eventFactory() {
+   const event = new Events();
+   const realTimeSeatUpdate = new RealTimeSeatUpdate();
+   const payment = new Payment();
+   const notification = new Notification();
+   return new EventManagement(
+     event,
+     realTimeSeatUpdate,
+     payment,
+     notification
+   );
+ }
+ 
+ const eventMangement = eventFactory();
